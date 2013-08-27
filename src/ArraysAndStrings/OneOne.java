8694e6811c64e8b9dde0077283fc709823ab1f2e
lastforeverzl <lastforeverzl@gmail.com>
2013-08-26 20:37:38 -0400

    1.1, 1.2

--- src/ArraysAndStrings/OneOne.java
+++ src/ArraysAndStrings/OneOne.java
@@ -1,5 +1,29 @@
+/*
+ * 1.1 Implement an algorithm to determine if a string has all unique characters. What if
+ * you can not use additional data structures?
+ */
+
 package ArraysAndStrings;
 
+import java.util.Scanner;
+
 public class OneOne {
+	public static void main(String[] args){
+		String input = "Helo";
+		if(isUniqueChar(input))
+			System.out.println("has all unique characters");
+		else
+			System.out.println("has duplicated characters");
+	}
 	
+	public static boolean isUniqueChar(String str){
+		int[] arr = new int[256];
+		for(int i=0;i<str.length();i++){
+			int val = str.charAt(i);
+			if(arr[val]==1)
+				return false;
+			arr[val]=1;
+		}
+		return true;
+	}
 }
